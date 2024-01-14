18146f3eea2a970f398405710552a8a70e93a919
SKG1191 <142608161+SKG1191@users.noreply.github.com>
2024-01-14 16:35:13 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "suresh@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+
+// accountId = 2
+accountEmail = "dc@mc.com"
+accountPassword = "343432"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
